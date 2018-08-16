c83011f33831fe63386a45d6943e99e4393c31a1
Marko Požega <markopozega@floatingpoint.io>
2018-08-16 15:44:53 +0200

    Add do block for has_many macro

--- lib/crudimentary/absinthe/generators/field.ex
+++ lib/crudimentary/absinthe/generators/field.ex
@@ -1,13 +1,15 @@
 defmodule CRUDimentary.Absinthe.Generator.Field do
   @moduledoc false
 
-  defmacro has_many(name) do
+  defmacro has_many(name, do: do_block) do
     singularized_name = Inflex.singularize(name)
     quote do
       field unquote(name), unquote(String.to_atom("#{singularized_name}_list_result")) do
         arg :filter, list_of(unquote(String.to_atom("#{singularized_name}_filter")))
         arg :sorting, unquote(String.to_atom("#{singularized_name}_sorting"))
         arg :pagination, :pagination_input
+
+        unquote(do_block)
       end
     end
   end
