f7a3f0f04ff00973963a1552dcc3276e0640e032
Marko Požega <markopozega@floatingpoint.io>
2018-08-24 14:10:46 +0200

    Handle exception when object_resolver fun is missing from gql type definition

--- lib/crudimentary/absinthe/middlewares/default.ex
+++ lib/crudimentary/absinthe/middlewares/default.ex
@@ -1,13 +1,25 @@
 defmodule CRUDimentary.Absinthe.Middlewares.Default do
   def call(%{source: source} = resolution, %{field: field, object: object}) do
     value =
-      unless String.ends_with?(object.name, "Result") do
-        {:ok, value} =
-          object.__reference__.module.object_resolver.call(source, field.args, resolution)
+      with \
+        object_resolver when not is_nil(object_resolver) <- extract_object_resolver(object),
+        false <- String.ends_with?(object.name, "Result"),
+        {:ok, value} <- object_resolver.(source, field.args, resolution)
+      do
         value
       else
-         Map.get(source, field.identifier)
+        _ -> Map.get(source, field.identifier)
       end
+
     %{resolution | state: :resolved, value: value}
+  end
+
+  defp extract_object_resolver(object) do
+    object_module = object.__reference__.module
+    if Keyword.has_key?(object_module.__info__(:functions), :object_resolver) do
+      &object.__reference__.module.object_resolver.call/3
+    else
+      nil
+    end
   end
 end
