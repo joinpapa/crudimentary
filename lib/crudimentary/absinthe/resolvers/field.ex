9771e3e517186d958ab7108fda58636fcbfb6ea7
Marko Požega <markopozega@floatingpoint.io>
2018-08-31 21:08:50 +0200

    Revert to old field resolver

--- lib/crudimentary/absinthe/resolvers/field.ex
+++ lib/crudimentary/absinthe/resolvers/field.ex
@@ -11,35 +11,20 @@ defmodule CRUDimentary.Absinthe.Resolvers.Field do
             args,
             %{definition: %{schema_node: %{identifier: field}}} = resolution
           ) do
-        with policy when not is_nil(policy) <-
-               unquote(params[:policy]) || policy_module(parent.__struct__),
-             true <- field in policy.accessible_attributes(parent, current_account),
-             external_resolver <- unquote(__MODULE__).get_external_resolver(field),
-             {:external, true} <- {:external, Code.ensure_compiled?(external_resolver)} do
-          external_resolver.call(current_account, parent, args, resolution)
-        else
-          {:external, false} ->
-            try do
-              call(field, current_account, parent, args, resolution)
-            rescue
-              FunctionClauseError ->
-                unquote(__MODULE__).return_field(parent, field)
-            end
+        policy = unquote(params[:policy]) || policy_module(parent.__struct__)
 
-          _ ->
-            {:ok, nil}
+        if field in policy.accessible_attributes(parent, current_account) do
+          try do
+            call(field, current_account, parent, args, resolution)
+          rescue
+            FunctionClauseError ->
+              unquote(__MODULE__).return_field(parent, field)
+          end
+        else
+          {:ok, nil}
         end
       end
     end
-  end
-
-  def get_external_resolver(field) do
-    submodule =
-      field
-      |> Atom.to_string()
-      |> Macro.camelize()
-
-    Module.concat([__MODULE__, submodule])
   end
 
   def return_field(struct, field) do
