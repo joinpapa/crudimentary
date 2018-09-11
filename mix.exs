2947ee6f91a4360192edf179bc71b0548992d05e
Marko Požega <markopozega@floatingpoint.io>
2018-09-11 15:16:18 +0200

    Bump up version do 0.1.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CRUDimentary.MixProject do
   def project do
     [
       app: :crudimentary,
-      version: "0.1.6",
+      version: "0.1.8",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
